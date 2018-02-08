c79b947fa9f44ef23a5e7b8df3299b579608bc2d
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-02-08 17:16:35 +0000

    automatically backed up by learn

--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -1,8 +1,8 @@
 class CreateGenres < ActiveRecord::Migration
 
-  def change 
+  def change
     create_table :genres do |t|
-      t.string :name 
-    end 
+      t.string :name
+    end
   end 
 end
